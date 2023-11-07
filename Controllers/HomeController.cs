59653833409157b5d5b93e99d8bc1fbfd824f8b5
Shogo Ohe <shogo.ohe.47@gmail.com>
2023-11-08 00:39:09 +0900

    add AppLogs messages.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,6 +15,11 @@ namespace azure_appservice_logging_netframework4.Controllers
 
         public ActionResult Logging()
         {
+            string className = GetType().Name;
+            string methodName = System.Reflection.MethodBase.GetCurrentMethod().Name;
+            System.Diagnostics.Trace.TraceInformation($"{className}#{methodName}: Logging page is called. records from from System.Diagnostics.Trace.TraceInformation() method.");
+            System.Diagnostics.Trace.TraceError($"{className}#{methodName}: Logging page is called. records from from System.Diagnostics.Trace.TraceError() method.");
+            Console.WriteLine($"{className}#{methodName}: Logging page is called. records from from Console.WriteLine() method.");
             return View();
         }
 
