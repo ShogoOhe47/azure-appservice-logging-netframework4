a7587c9fa9def05e4894d6fe4979772c8b351782
Shogo Ohe <shogo.ohe.47@gmail.com>
2023-12-05 12:04:57 +0900

    update Logging, Diagnostics

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.WebPages;
 
 namespace azure_appservice_logging_netframework4.Controllers
 {
@@ -17,8 +18,14 @@ namespace azure_appservice_logging_netframework4.Controllers
         {
             string className = GetType().Name;
             string methodName = System.Reflection.MethodBase.GetCurrentMethod().Name;
-            System.Diagnostics.Trace.TraceInformation($"{className}#{methodName}: Logging page is called. records from from System.Diagnostics.Trace.TraceInformation() method.");
-            System.Diagnostics.Trace.TraceError($"{className}#{methodName}: Logging page is called. records from from System.Diagnostics.Trace.TraceError() method.");
+            if (System.Environment.GetEnvironmentVariable("WEBSITE_SITE_NAME").IsEmpty() == false)
+            {
+                // this code only running on Azure
+                // System.Diagnostics.Trace.TraceInformation is logging to AppServiceLog:ApplicationLog
+                System.Diagnostics.Trace.TraceInformation($"{className}#{methodName}: Logging page is called. records from from System.Diagnostics.Trace.TraceInformation() method.");
+                // System.Diagnostics.Trace.TraceInformation is logging to AppServiceLog:ApplicationLog, DiagnosticsSettings:AppServiceAppLogs
+                System.Diagnostics.Trace.TraceError($"{className}#{methodName}: Logging page is called. records from from System.Diagnostics.Trace.TraceError() method.");
+            }
             Console.WriteLine($"{className}#{methodName}: Logging page is called. records from from Console.WriteLine() method.");
             return View();
         }
